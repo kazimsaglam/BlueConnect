da239f0eb1292b0b7178b5101b24e8fe900a8232
Kazim <Kazimsaglamdev@gmail.com>
2025-04-04 17:27:32 +0300

    second commit

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const path = require("path");
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 let latestData = {};
 
